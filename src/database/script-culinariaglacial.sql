5a434813b04c03be5bb20cdb30ae3b59b1b2a9f8
GustavoAlvesDeOliveira <gustavo.aoliveira@sptech.school>
2025-06-04 20:18:01 -0300

    Novo banco de dados reduzido

--- src/database/script-culinariaglacial.sql
+++ src/database/script-culinariaglacial.sql
@@ -1,94 +1,58 @@
-Drop database if exists culinariaglacial;
 
+-- Organizando o ambiente
+
+drop database if exists culinariaglacial;
 create database culinariaglacial;
-
 use culinariaglacial;
 
+-- Criando as tabelas
 
--- Tabela cadastro
-
-
-CREATE TABLE usuario (
-  id INT AUTO_INCREMENT,
-  Nome VARCHAR(30) NOT NULL,
-  sobrenome VARCHAR(60) NULL,
-  Idade DATE NULL,
-  genero VARCHAR(20) NULL,
-  Email VARCHAR(100) NULL,
-  Senha VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+create table usuario (
+  id int not null auto_increment,
+  Nome varchar(30),
+  Sobrenome varchar(60) ,
+  Idade date,
+  Genero varchar(20),
+  Email varchar(100),
+  Senha varchar(30),
+  primary key (id)
 );
 
-
--- Tabela das paginas e sistema de favoritos
-
-
-CREATE TABLE Paginas_Receitas (
-  id INT AUTO_INCREMENT NOT NULL,
-  Nome VARCHAR(45) NULL,
-  PRIMARY KEY (id)
+create table quiz(
+	idquiz int not null,
+    descricao varchar(100),
+    primary key (idquiz)
 );
 
-
-CREATE TABLE Comentarios (
-	id INT AUTO_INCREMENT NOT NULL,
-	FKUsuario INT NOT NULL,
-	FKPaginas_Receitas INT NOT NULL,
-	Conteudo VARCHAR(150) NULL,
-	PRIMARY KEY (id, FKUsuario, FKPaginas_Receitas),
-	CONSTRAINT fk_Comentarios_Usuario
-	FOREIGN KEY (FKUsuario)
-	REFERENCES usuario(id),
-	CONSTRAINT fk_Comentarios_Paginas_Receitas
-	FOREIGN KEY (FKPaginas_Receitas)
-	REFERENCES Paginas_Receitas(id)
+create table  resultado_quiz(
+	fkusuario int not null,
+    fkquiz int not null,
+	emblema varchar(100),
+	titulo varchar(30),
+    acertos char(2),
+    erros char(2),
+    primary key (fkusuario,fkquiz),
+    foreign key (fkusuario) references usuario(id),
+    foreign key (fkquiz) references quiz(idquiz)
 );
 
+-- inserts 'chumbados'
+insert into quiz (idquiz,descricao)
+values ('1','Quiz de conhecimentos gerais do meu site.');
 
-CREATE TABLE Receitas_Favoritas (
-  FKUsuario INT NOT NULL,
-  FKPaginas_Receitas INT NOT NULL,
-  PRIMARY KEY (FKUsuario, FKPaginas_Receitas),
-  CONSTRAINT fk_Receitas_Favoritas_Usuario
-    FOREIGN KEY (FKUsuario)
-    REFERENCES usuario(id),
-  CONSTRAINT fk_Receitas_Favoritas_Paginas_Receitas
-    FOREIGN KEY (FKPaginas_Receitas)
-    REFERENCES Paginas_Receitas(id)
-);
+-- Teste em selects:
 
+select * from usuario;
+select * from quiz;
+select * from resultado_quiz;
 
--- Tabelas do quiz
+-- Juntando tudo.
 
+Select * from usuario
+join resultado_quiz 
+on fkusuario = id 
+join quiz 
+on fkquiz = idquiz;
 
-CREATE TABLE Respostas (
-  idRespostas INT AUTO_INCREMENT NOT NULL,
-  Conteudo VARCHAR(50) NULL,
-  PRIMARY KEY (idRespostas)
-  );
+-- selects do meu projeto individual
 
-
-CREATE TABLE Perguntas (
-  idPerguntas INT AUTO_INCREMENT NOT NULL,
-  Conteudo VARCHAR(150) NULL,
-  PRIMARY KEY (idPerguntas)
-);
-
-
-CREATE TABLE Perguntas_has_Respostas (
-  FKPerguntas INT NOT NULL,
-  FKRespostas INT NOT NULL,
-  FKUsuario INT NOT NULL,
-  PRIMARY KEY (FKPerguntas, FKRespostas, FKUsuario),
-  CONSTRAINT fk_Perguntas_has_Respostas_Perguntas1
-    FOREIGN KEY (FKPerguntas)
-    REFERENCES Perguntas (idPerguntas),
-  CONSTRAINT fk_Perguntas_has_Respostas_Respostas1
-    FOREIGN KEY (FKRespostas)
-    REFERENCES Respostas(idRespostas),
-  CONSTRAINT fk_Perguntas_has_Respostas_Usuario1
-    FOREIGN KEY (FKUsuario)
-    REFERENCES usuario(id)
-);
-
-select * from culinariaglacial.usuario;
